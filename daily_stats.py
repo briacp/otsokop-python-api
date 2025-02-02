80757b9a6b1f4f6e45a3cfb6bafeaa28f0371d3c
Briac Pilpré <briac.pilpre@blablacar.com>
2025-02-02 15:37:56 +0100

    données mensuelles et quotidiennes

--- daily_stats.py
+++ daily_stats.py
@@ -4,43 +4,147 @@ from datetime import datetime
 from dateutil.relativedelta import relativedelta
 from otsokop.odoo import Odoo
 from otsokop.odoo import banner as otsokop_banner
+import logging
+import holidays
+from babel.dates import format_date
 
-offline = False
-date_start = sys.argv[1] if len(sys.argv) >= 2 else (date.today() + relativedelta(days=-2)).strftime("%Y-%m-%d")
-date_end = sys.argv[2] if len(sys.argv) >= 3 else date_start
 
-client = Odoo("../../assets/cfg/app_settings.json")
+with_banner = True
+fr_holidays = holidays.FR()
 
-prod_racks = client.products_by_racks()
-print(prod_racks)
-sys.exit(12)
 
-order_dataframes = client.get_pos_orders(date_start, date_end)
-orders = order_dataframes[0]
-lines = order_dataframes[1]
+def main():
+    date_start = (
+        sys.argv[1]
+        if len(sys.argv) >= 2
+        else (date.today() + relativedelta(days=-1)).strftime("%Y-%m-%d")
+    )
+    date_start = datetime.strptime(date_start, "%Y-%m-%d")
 
-date_start = datetime.strptime(date_start, "%Y-%m-%d")
-print(otsokop_banner)
-print("")
-print("Résumé -", date_start.strftime("%A %Y-%m-%d"))
+    daily_stats(date_start)
 
-if orders.empty:
-    print("(aucune donnée disponible)")
-else:
-    print("  Passages    :", orders["id"].count())
-    print("  Total       :", round(orders["amount_total"].sum(), 2), "€")
-    print("  Panier Moy. :", round(orders["amount_total"].mean(), 2), "€")
-    print("  Panier Med. :", round(orders["amount_total"].median(), 2), "€")
-print("")
+    if date_start.day == 1:
+        monthly_stats(datetime.strptime("2025-01", "%Y-%m"))
 
-print("---")
-print(orders.sort_values(by="create_date"))
-print("---")
 
-top_sales = (
-    lines.groupby(["product_name", "product_id", "discount"])
-    .agg({"price_subtotal_incl": "sum", "qty": "sum"})
-    .sort_values(by="price_subtotal_incl", ascending=False)
-    .head(20)
-)
-print(top_sales)
+def daily_stats(current_date):
+
+    if current_date in fr_holidays:
+        print(
+            f"Le {format_date(current_date, locale='fr_FR')} est un jour ferié ({fr_holidays.get(current_date)}), le magasin était fermé"
+        )
+        return 1
+    if current_date.weekday() == 6:
+        print(
+            f"Le {format_date(current_date, locale='fr_FR')} est un dimanche, le magasin était fermé"
+        )
+        return 2
+
+    client = Odoo("../../assets/cfg/app_settings.json", logging.INFO)
+    order_dataframes = client.get_pos_orders(current_date.strftime("%Y-%m-%d"))
+    orders = order_dataframes[0]
+
+    if with_banner:
+        print(otsokop_banner)
+        print("")
+    print(
+        "Ventes pour le", format_date(current_date, "EEEE dd MMM yyyy", locale="fr_FR")
+    )
+
+    current_year_sales = orders["amount_total"].sum()
+
+    if orders.empty:
+        print("(aucune donnée disponible)")
+    else:
+        print(order_summary(orders))
+    print("")
+
+    last_year_date = current_date + relativedelta(
+        years=-1, weekday=current_date.weekday()
+    )
+
+    is_holiday = last_year_date in fr_holidays
+
+    order_dataframes = client.get_pos_orders(last_year_date.strftime("%Y-%m-%d"))
+    orders = order_dataframes[0]
+
+    print(
+        "Ventes pour le",
+        format_date(last_year_date, "EEEE dd MMM yyyy", locale="fr_FR"),
+    )
+    if is_holiday:
+        print(f"** Jour ferié ({fr_holidays.get(last_year_date)})")
+
+    if orders.empty:
+        print("(aucune donnée disponible)")
+    else:
+        print(order_summary(orders))
+        print("")
+        last_year_sales = orders["amount_total"].sum()
+        print(
+            f"Indice de réalisation (CA actuel / CA année dernière) : {current_year_sales / last_year_sales * 100:.02f}%"
+        )
+    print("")
+
+
+def monthly_stats(current_date):
+
+    end_date = current_date + relativedelta(months=1)
+
+    client = Odoo("../../assets/cfg/app_settings.json", logging.INFO)
+    order_dataframes = client.get_pos_orders(
+        current_date.strftime("%Y-%m-%d"), end_date.strftime("%Y-%m-%d")
+    )
+    orders = order_dataframes[0]
+
+    if with_banner:
+        print("")
+
+    print(
+        "Ventes pour le mois de", format_date(current_date, "MMMM yyyy", locale="fr_FR")
+    )
+
+    current_year_sales = orders["amount_total"].sum()
+
+    if orders.empty:
+        print("(aucune donnée disponible)")
+    else:
+        print(order_summary(orders))
+    print("")
+
+    last_year_date = current_date + relativedelta(years=-1)
+    end_date = last_year_date + relativedelta(months=1)
+
+    order_dataframes = client.get_pos_orders(
+        last_year_date.strftime("%Y-%m-%d"), end_date.strftime("%Y-%m-%d")
+    )
+    orders = order_dataframes[0]
+
+    print(
+        "Ventes pour le mois de",
+        format_date(last_year_date, "MMMM yyyy", locale="fr_FR"),
+    )
+
+    if orders.empty:
+        print("(aucune donnée disponible)")
+    else:
+        last_year_sales = orders["amount_total"].sum()
+        print(order_summary(orders))
+        print("")
+        print(
+            f"Indice de réalisation (CA actuel / CA année dernière) : {current_year_sales / last_year_sales * 100:.02f}%"
+        )
+    print("")
+
+
+def order_summary(orders):
+    print(f"  * Chiffre d'affaire    : {orders['amount_total'].sum():.02f} €")
+    print(f"  * Panier moyen         : {orders['amount_total'].mean():.02f} €")
+    print(f"  * Panier median        : {orders['amount_total'].median():.02f} €")
+    print(f"  * Nombre de commandes  : {orders['id'].count()}")
+    print(f"  * Coops/Acheteurs      : {orders['partner_id'].nunique()}")
+    return ""
+
+
+if __name__ == "__main__":
+    sys.exit(main())
